62a3cb2b3e6fae85346ced91e6dede80fd752f19
Akanksha7083 <akankshadhami37@gmail.com>
2023-11-05 22:46:51 +0530

    deleted

--- first.java
+++
@@ -1,7 +0,0 @@
-class a
-{
-public static void main(String st[]){                  //if not specified any access specifier then it will be default by default and not public
-System.out.println("welcome to java");          //system:class; out:object of of PrintStream class; println() is the function in the class PrintStream
-
-}
-}
